ae759529dfd1161345c01f4bcc622be57fc88ea3
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-31 19:46:23 -0400

    added icon and full forecast day 1 display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //global variable declarations
 var textInput = document.querySelector(".search-box");
 var citySearch = "";
-console.log(textInput);
 var searchedCitylat = "";
 var searchedCitylong = "";
+var forecastLength = 5;
 
 function getForecastApi(city){
-  //
+  //create second API call to generate 5 day forecast and UV index
   fetch("https://api.openweathermap.org/data/2.5/onecall?" 
   + "lat=" + searchedCitylat
   + "&lon=" + searchedCitylong
@@ -28,7 +28,51 @@ function getForecastApi(city){
 
   .then(function(data) {
     console.log(data);
+    //add UV index to hero
     $(".uv-index").text("UV Index " + data.current.uvi);
+
+      var forecastDay1 = document.createElement("div");
+      forecastDay1.classList = "col-4 card day1";
+
+      var forecastdate = document.createElement("h3");
+      forecastdate.classList = "day1";
+      forecastdate.textContent = new Date(data.daily[0].dt*1000);
+      forecastDay1.appendChild(forecastdate);
+
+      var forecastIcon = document.createElement("img");
+      forecastIcon.classList = "icon";
+      forecastIcon.src = "http://openweathermap.org/img/wn/" 
+      + data.daily[0].weather[0].icon
+      + "@2x.png"
+      forecastDay1.appendChild(forecastIcon);
+
+      var forecastTemp = document.createElement("h4");
+      forecastTemp.classList = "day1";
+      forecastTemp.textContent = "Temperature" + data.daily[0].temp.max;
+      forecastDay1.appendChild(forecastTemp);
+
+      var forecastWind =document.createElement("h4");
+      forecastWind.classList = "day1";
+      forecastWind.textContent = "Wind: " + data.daily[0].wind_speed;
+      forecastDay1.appendChild(forecastWind);
+
+      var forecastHumidity =document.createElement("h4");
+      forecastHumidity.classList = "day1";
+      forecastHumidity.textContent = "Humidity: " + data.daily[0].humidity;
+      forecastDay1.appendChild(forecastHumidity);
+
+      document.querySelector(".city-forecast").appendChild(forecastDay1);
+
+
+    //add forecast data to respective div elements
+    // $(".day1").add("h3").addClass("day-1-date").text(data.daily[0].dt);
+
+
+    // addClass("forecast-1-date");
+    // $(".forecast-1-date").text(data.daily[0].dt);
+    // $("n1").append(this);
+    // $("#n1").add("h4").addClass("forecast-1-temp").text(data.daily[0].temp.max);
+    // $("n1").append(this);
 
 })
 };
