98cd7db1d088c4c086b9ab2d28677b4aaeb0158d
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-31 09:24:49 -0400

    populated daily forecast values excl uv

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var textInput = document.querySelector(".search-box");
 var citySearch = "";
 console.log(textInput);
 
+function displayFiveDay(city){
+  // 
+}
 var searchSubmitHandler = function(event) {
     // prevent page from refreshing
   event.preventDefault();
@@ -12,6 +15,7 @@ var searchSubmitHandler = function(event) {
 
   if(citySearch) {
       getWeatherApi(citySearch);
+
   } else {
       alert("Please input a city name");
   }
@@ -20,25 +24,43 @@ var searchSubmitHandler = function(event) {
 
 var getWeatherApi = function() {
     // event.preventDefault();
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" +citySearch+ "&appid=4bdf76bd7f935435d6ca49a5f4268f54")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" 
+    + citySearch 
+    + "&units=metric"
+    + "&appid=4bdf76bd7f935435d6ca49a5f4268f54")
     .then(function(response) {
       // request was successful
       if (response.ok) {
         // console.log(response);
-        response.json().then(function(data) {
-          console.log(data);
-        });
+        return response.json();
+
       } else {
         alert('Error: ' + response.statusText);
       }
     })
     .catch(function(error) {
       alert('Unable to connect to OpenWeather API');
-    });
-    
+    })
 
-}
+    .then(function(data) {
+      displayFiveDay()
+      console.log(data)
+      //change city name header to city name
+      $(".city-hero-name").text(data.city.name);
+      //update temperature
+      $(".temperature").text("Temperature: " + data.list[0].main.temp + '°C');
+      $(".wind").text("Wind: " + data.list[0].wind.speed + 'km/h');
+      $(".humidity").text("Humidity: " + data.list[0].main.humidity + "%");
+      // $(".uv-index").text("UV Index: " + data.list[0].)
+    })
+  }
+
+
+    // var displayWeather = function() {
+    //     response.json().then(function(data)
+    // }
 
 //event handler for search function
 $("#search-now").on("click", searchSubmitHandler);
+
 
