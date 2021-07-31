98da87878f33af8c64222aee5ce98bff583a0f66
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-31 17:35:13 -0400

    connected to second API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,37 @@
 var textInput = document.querySelector(".search-box");
 var citySearch = "";
 console.log(textInput);
+var searchedCitylat = "";
+var searchedCitylong = "";
 
-function displayFiveDay(city){
-  // 
-}
+function getForecastApi(city){
+  //
+  fetch("https://api.openweathermap.org/data/2.5/onecall?" 
+  + "lat=" + searchedCitylat
+  + "&lon=" + searchedCitylong
+  + "&units=metric"
+  + "&appid=4bdf76bd7f935435d6ca49a5f4268f54")
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      // console.log(response);
+      return response.json();
+
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    alert('Unable to connect to OpenWeather API');
+  })
+
+  .then(function(data) {
+    console.log(data);
+    $(".uv-index").text("UV Index " + data.current.uvi);
+
+})
+};
+
 var searchSubmitHandler = function(event) {
     // prevent page from refreshing
   event.preventDefault();
@@ -43,8 +70,11 @@ var getWeatherApi = function() {
     })
 
     .then(function(data) {
-      displayFiveDay()
-      console.log(data)
+      console.log(data);
+      searchedCitylat = data.city.coord.lat;
+      searchedCitylong = data.city.coord.lon;
+      console.log(searchedCitylat);
+      console.log(searchedCitylong);
       //change city name header to city name
       $(".city-hero-name").text(data.city.name);
       //update temperature
@@ -52,6 +82,7 @@ var getWeatherApi = function() {
       $(".wind").text("Wind: " + data.list[0].wind.speed + 'km/h');
       $(".humidity").text("Humidity: " + data.list[0].main.humidity + "%");
       // $(".uv-index").text("UV Index: " + data.list[0].)
+      getForecastApi(citySearch);
     })
   }
 
